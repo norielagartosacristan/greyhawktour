322224cd36c9857faf45fdcdb0ea9d321d9c8b61
norielagartosacristan <norielagartosacristan@gmail.com>
2024-09-26 17:00:27 +0800

    Add
    Add

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '55e70e420dc590986791b97cc3fa615cb614b172',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '55e70e420dc590986791b97cc3fa615cb614b172',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'amadeus4dev/amadeus-php' => array(
+            'pretty_version' => '0.3.0',
+            'version' => '0.3.0.0',
+            'reference' => 'f7249b5659976ee838dee0222497e547937fba93',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../amadeus4dev/amadeus-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
